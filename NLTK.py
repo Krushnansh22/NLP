432f64aea7a848a158cebd5269c524fcb8633ec8
Krushnansh22 <krushnansh22@gmail.com>
2024-08-06 11:32:58 +0530

    NLP

---
+++ NLTK.py
@@ -0,0 +1,11 @@
+import nltk
+prompt = "Twinkle Twinkle Little Star how I wonder what you are. Up about the world so high like a diamond in the sky."
+print(f'Prompt :- {prompt}')
+
+# Tokenization
+tokens = nltk.word_tokenize(prompt)
+print(f"Tokenized prompt :- \n{tokens}")
+
+# POS Tagging
+tags = nltk.pos_tag(prompt)
+print(f'POS tagging : \n{tags}')
