bb3ec0a968987f6e66315decd51f6f666c3fe5e6
Krushnansh22 <krushnansh22@gmail.com>
2024-08-17 22:15:49 +0530

    commited

--- NLTK.py
+++ NLTK.py
@@ -8,4 +8,14 @@ print(f"Tokenized prompt :- \n{tokens}")
 
 # POS Tagging
 tags = nltk.pos_tag(tokens)
-print(f'POS tagging : \n{tags}')
+print(f'POS tagging : \n{tags}\n')
+
+# named entities 
+print ('entities:-')
+entity = nltk.chunk.ne_chunk(tags)
+print(f"{entity}")
+
+# Parsing tree
+from nltk.corpus import treebank
+t = treebank.parsed_sents('wsj_0001.mrg')[1]
+t.draw()
